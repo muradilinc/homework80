12e5c8edf585f660030d94a515bb4ec47a83b12e
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-02-01 19:21:54 +0600

    delete item from database

--- routes/inventors.ts
+++ routes/inventors.ts
@@ -27,7 +27,7 @@ inventorsRouter.post('/', imagesUpload.single('image'), async (req, res, next) =
       id: result.insertId,
       ...item,
     });
-  }catch (error) {
+  } catch (error) {
     return next(error);
   }
 });
@@ -59,5 +59,55 @@ inventorsRouter.get('/:id', async (req, res, next) => {
     return next(error);
   }
 });
+
+inventorsRouter.put('/:id', imagesUpload.single('image'), async (req, res, next) => {
+  try {
+    const item: Item = {
+      name: req.body.name,
+      categoryId: parseInt(req.body.categoryId),
+      locationId: parseInt(req.body.locationId),
+      description: req.body.description,
+      delivery: req.body.delivery,
+      image: req.file ? req.file.filename : null,
+    };
+    const fields: Item[] = [];
+
+    Object.entries(item).forEach(([key , value]) => {
+      console.log(typeof key);
+      if (value) {
+        fields.push(value);
+      }
+    });
+
+    // const [results] = await mysqlDb.getConnection().query(
+    //   'UPDATE item SET ? WHERE id = ?',
+    //   [fields, req.params.id],
+    // ) as RowDataPacket[];
+
+    // const item = results[0];
+    //
+    // if (!item) {
+    //   return res.status(404).send({error: 'Not found !'});
+    // }
+    console.log(fields);
+
+    res.send(fields);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+inventorsRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const results = await mysqlDb.getConnection().query(
+      'DELETE FROM item WHERE id = ?',
+      [req.params.id],
+    ) as ResultSetHeader[];
+
+    res.send('delete item ' + req.params.id);
+  } catch (error) {
+    return next(error);
+  }
+})
 
 export default inventorsRouter;
